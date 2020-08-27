c718ec56bf141617fcb139b8154ac050e46c83df
matschreiner <jamas@pop-os.localdomain>
2020-08-27 13:56:07 +0200

    recurse into directories

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -4,40 +4,40 @@ create_symlinks(){
 # $1 Target dir
 # $2 Link dir
 
-for FILE in $(ls $1); do
+for FILE in  $(ls $1); do
 
 	if [[ $FILE == *.symlink ]]; then # File should be symlinked
-		TARGETPATH=$(realpath $1/$FILE)
+		TARGETPATH=$(realpath "$1"/"$FILE")
 		LINKNAME=${FILE%".symlink"}
 		LINKNAME=${LINKNAME/"dot."/"."}
-		LINKPATH=$2/$LINKNAME
+		LINKPATH="$2"/"$LINKNAME"
 
-		if [[ -d $TARGETPATH ]]; then # if file is a dir
+		if [[ -d "$TARGETPATH" ]]; then # if file is a dir
 			echo "$TARGETPATH is dir and should be copied to $LINKNAME"
 
-			if [[ ! -e $LINKPATH ]]; then
-				mkdir $LINKPATH
+			if [[ ! -e "$LINKPATH" ]]; then
+				mkdir "$LINKPATH"
 			fi
 
-			create_symlinks $TARGETPATH $LINKPATH
+			create_symlinks "$TARGETPATH" "$LINKPATH"
 
 
-		elif [[ -e $LINKPATH ]]; then # if linkpath exists
-			if [[ -L $LINKPATH ]]; then # linkpath is link
-				if [[ $(realpath $LINKPATH) == $TARGETPATH ]]; then
+		elif [[ -e "$LINKPATH" ]]; then # if linkpath exists
+			if [[ -L "$LINKPATH" ]]; then # linkpath is link
+				if [[ $(realpath "$LINKPATH") == "$TARGETPATH" ]]; then
 					echo "$LINKNAME is already set up properly"
 				# code for recursion belonss here *
 				fi
 			fi
 		else # if linkpath doesn't exist
-			ln -s $TARGETPATH $LINKPATH
+			ln -s "$TARGETPATH" "$LINKPATH"
 			echo "Symlink set up for $LINKNAME"
 		fi
 	fi
 done
 }
 
-create_symlinks $HOME/.dotfiles $HOME
+create_symlinks "$HOME"/.dotfiles "$HOME"
 echo "Symlinks created to dotfiles"
 
 
--- scripts/install_vundle.sh
+++ scripts/install_vundle.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git vim.symlink/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.dotfiles/vim.symlink/bundle/Vundle.vim
