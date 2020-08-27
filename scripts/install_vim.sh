c74e4e9bf069fd6a944e84f5b6d53d5d7e4d56f4
matschreiner <matschreiner@gmail.com>
2020-08-27 14:40:56 +0000

    Remove sudo from installscript

--- scripts/install_vim.sh
+++ scripts/install_vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install vim
+apt-get install vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +VimEnter +PluginInstall +qall
