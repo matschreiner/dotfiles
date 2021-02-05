c0876ee9c9b65822dce0d59a6efb71cb506e4874
Mathias Schreiner <jamas@dtu.dk>
2021-02-05 13:39:25 +0100

    Don't activate venvs

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 tmux new-session -d
-tmux send-keys 'source .venv/bin/activate' C-m
-
-tmux split-window -v -p 15
-tmux send-keys 'source .venv/bin/activate' C-m
-
+tmux split-window -v -p 1
 tmux split-window -h
-tmux send-keys 'source .venv/bin/activate' C-m
 
 tmux -2 attach-session -d
