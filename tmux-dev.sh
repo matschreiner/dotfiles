b9f7b8b4870f3b27151c29f44e544306ad5fe20c
Mathias Schreiner <jamas@dtu.dk>
2021-09-03 15:13:00 +0200

    write script to open tmux in dev mode

--- tmux-dev.sh
+++ tmux-dev.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-tmux new-session -d
-tmux split-window -v -p 1 -c $PWD
-tmux split-window -h -c $PWD
 
-tmux -2 attach-session -d
+function activate_venv () {
+	if [[ -d ./.venv ]]
+	then
+		tmux send-keys 'source .venv/bin/activate' Enter
+	fi
+}
+
+
+if [[ -s $1 ]]
+then
+	cd $1
+fi
+
+tmux send-keys nvim Enter Enter \; send-keys C-n \; send-keys C-l
+
+tmux split-window -v
+activate_venv
+
+tmux resize-pane -y 18
+tmux split-window -h
+activate_venv
+
+
