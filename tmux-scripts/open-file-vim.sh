0a6c96434d9c0f3d16b042ba7f6928db8cb95125
Mathias Schreiner <jamas@dtu.dk>
2021-10-04 15:29:52 +0200

    always split right so we don't interfere with nerdtree

--- tmux-scripts/open-file-vim.sh
+++ tmux-scripts/open-file-vim.sh
@@ -2,7 +2,8 @@
 
 file=$(pbpaste)
 
-tmux send-keys -t 0 "set splitright"
+tmux send-keys -t 0 Escape
+tmux send-keys -t 0 ":set splitright"
+tmux send-keys -t 0 Enter
 tmux send-keys -t 0 ":vsplit $file"
 tmux send-keys -t 0 Enter
-
