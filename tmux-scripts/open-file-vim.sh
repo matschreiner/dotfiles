5b6aba5e2c1638984f271d94738672dc2b773d87
Mathias Schreiner <jamas@dtu.dk>
2021-10-05 11:51:05 +0200

    move right and select vim after opening file

--- tmux-scripts/open-file-vim.sh
+++ tmux-scripts/open-file-vim.sh
@@ -2,8 +2,17 @@
 
 file=$(pbpaste)
 
+
 tmux send-keys -t 0 Escape
+
+tmux send-keys -t 0 C-r
+tmux send-keys -t 0 C-r
+tmux send-keys -t 0 C-r
+tmux send-keys -t 0 C-r
+tmux send-keys -t 0 C-r
+
 tmux send-keys -t 0 ":set splitright"
 tmux send-keys -t 0 Enter
 tmux send-keys -t 0 ":vsplit $file"
 tmux send-keys -t 0 Enter
+tmux select-pane -t 0
