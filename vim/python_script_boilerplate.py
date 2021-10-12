e20c27d28261cd6da55e781cc1e01f9aa23a702a
Mathias Schreiner <jamas@dtu.dk>
2021-10-12 14:39:11 +0200

    add boilerplate for parser code

---
+++ vim/python_script_boilerplate.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument()
+    args = parser.parse_args()
+
+    main(args)
